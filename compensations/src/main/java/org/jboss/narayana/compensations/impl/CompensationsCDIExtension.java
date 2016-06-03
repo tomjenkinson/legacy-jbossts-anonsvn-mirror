jbosstm/narayana

[JBTM-2622] Possible new compensations api from the meeting

compensations/src/main/java/org/jboss/narayana/compensations/impl/CompensationsCDIExtension.java
@@ -54,6 +54,8 @@ public class CompensationsCDIExtension implements Extension {
         bbd.addAnnotatedType(bm.createAnnotatedType(TxConfirmInterceptor.class));
         bbd.addAnnotatedType(bm.createAnnotatedType(TxLoggedInterceptor.class));
         bbd.addAnnotatedType(bm.createAnnotatedType(CancelOnFailureInterceptor.class));
+
+        bbd.addAnnotatedType(bm.createAnnotatedType(TransactionProducer.class));
     }
 
     public void afterBeanDiscovery(@Observes AfterBeanDiscovery event, BeanManager manager) {

tomjenkinson/legacy-jbossts-anonsvn-mirror

compensations/src/main/java/org/jboss/narayana/compensations/impl/CompensationsCDIExtension.java
@@ -54,6 +54,7 @@ public class CompensationsCDIExtension implements Extension {
         bbd.addAnnotatedType(bm.createAnnotatedType(TxConfirmInterceptor.class));
         bbd.addAnnotatedType(bm.createAnnotatedType(TxLoggedInterceptor.class));
         bbd.addAnnotatedType(bm.createAnnotatedType(CancelOnFailureInterceptor.class));
+        bbd.addAnnotatedType(bm.createAnnotatedType(CompensatableActionProducer.class));
     }
 
     public void afterBeanDiscovery(@Observes AfterBeanDiscovery event, BeanManager manager) {